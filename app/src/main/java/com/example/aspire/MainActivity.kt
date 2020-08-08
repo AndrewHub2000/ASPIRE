e08a1ade3ad57b0d774c5dc40993c40b4ac89038
AndrewGitLab2000 <alekseevandrej225@gmail.com>
2020-08-08 21:50:43 +0300

    04.08.2020

--- app/src/main/java/com/example/aspire/MainActivity.kt
+++ app/src/main/java/com/example/aspire/MainActivity.kt
@@ -10,9 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener{
-            val intent = Intent (this, MainActivity2::class.java)
-            startActivity(intent)
-        }
     }
 }
