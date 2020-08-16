5ceb6bef0a0141a1085d56b4e396aa4a3b7bed77
AndrewGitLab2000 <alekseevandrej225@gmail.com>
2020-08-16 23:08:37 +0300

    16.08.2020

--- app/src/main/java/com/example/aspire/MainActivity4.kt
+++ app/src/main/java/com/example/aspire/MainActivity4.kt
@@ -1,11 +1,18 @@
 package com.example.aspire
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main4.*
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
+
+        button4.setOnClickListener{
+            val intent = Intent(this, MainPage::class.java)
+            startActivity(intent)
+        }
     }
 }
