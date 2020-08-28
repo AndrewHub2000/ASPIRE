bbd28ec56017c4492aa4cd3682b192eb7ad139bd
AndrewGitLab2000 <alekseevandrej225@gmail.com>
2020-08-28 20:03:01 +0300

    28.08.2020

--- app/src/main/java/com/example/aspire/MainPage.kt
+++ app/src/main/java/com/example/aspire/MainPage.kt
@@ -2,27 +2,10 @@ package com.example.aspire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-
 
 class MainPage : AppCompatActivity() {
-    var emptyLike: ImageView? = null
-    var is_filled = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
-        emptyLike = findViewById(R.id.like_empty)
-    }
-    fun changeToFilledLike(view: View)
-    {
-        view as ImageView
-        if (!is_filled) {
-            view.setImageResource(R.drawable.like_empty)
-            is_filled = true
-        } else {
-            view.setImageResource(R.drawable.like_filled)
-            is_filled = false
-        }
     }
 }
