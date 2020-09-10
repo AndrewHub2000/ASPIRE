8a19ed4f7ab650cc0eb1f4d34ef9e8f52787a6b3
AndrewGitLab2000 <alekseevandrej225@gmail.com>
2020-09-10 15:50:29 +0300

    10.09.2020

--- app/src/main/java/com/example/aspire/BottomNavigation.kt
+++ app/src/main/java/com/example/aspire/BottomNavigation.kt
@@ -1,11 +1,13 @@
 package com.example.aspire
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_bottom_navigation.*
 
 class BottomNavigation : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_navigation)
@@ -19,7 +21,7 @@ class BottomNavigation : AppCompatActivity() {
         setCurrentFragment(homeFragment)
 
         bottom_navigation_view.setOnNavigationItemSelectedListener {
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.home -> setCurrentFragment(homeFragment)
                 R.id.search -> setCurrentFragment(searchFragment)
                 R.id.account -> setCurrentFragment(accountFragment)
