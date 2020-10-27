1125307612a9a5645f8106982b9e94793ec22ee8
AndrewGitLab2000 <alekseevandrej225@gmail.com>
2020-10-27 15:57:19 +0300

    21.10.2020

--- app/src/main/java/com/example/aspire/OrderingDescription.kt
+++ app/src/main/java/com/example/aspire/OrderingDescription.kt
@@ -3,9 +3,9 @@ package com.example.aspire
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ConfirmTheOrder : AppCompatActivity() {
+class OrderingDescription : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_confirm_the_order)
+        setContentView(R.layout.activity_ordering_description)
     }
 }
